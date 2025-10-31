75280bfaa74552a46058f7858cd427fcf7115614
Moritz344 <moritzmaier34@gmail.com>
2025-10-31 16:56:38 +0100

    fixed spacing

--- src/chat/chat.component.ts
+++ src/chat/chat.component.ts
@@ -1,75 +1,77 @@
-import { Component,Input,OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SettingsService } from '../services/settings.service';
+import { Component, Input, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SettingsService } from "../services/settings.service";
 
 @Component({
-  selector: 'app-chat',
+  selector: "app-chat",
   imports: [CommonModule],
-  templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  templateUrl: "./chat.component.html",
+  styleUrl: "./chat.component.css",
 })
-export class ChatComponent implements OnInit{
-				@Input() message: string = "";
+export class ChatComponent implements OnInit {
+  @Input() message: string = "";
 
-				currentDate: string = "";
-				currentName: string = ""
-				currentMessage: string = "";
-				userColorArray: string[]  = [
-								"#FFA500", // Orange
-    						"#FF4500", // Orangered
-    						"#FF0000", // Red
-    						"#FFFF00", // Yellow
-    						"#00FF00", // Lime
-    						"#0000FF", // Blue
-    						"#8A2BE2", // BlueViolet
-    						"#4B0082", // Indigo
-    						"#FF69B4", // HotPink
-    						"#D2691E", // Chocolate
-    						"#FF8C00", // DarkOrange
-    						"#ADFF2F", // GreenYellow
-    						"#20B2AA", // LightSeaGreen
-    						"#FFD700", // Gold
-    						"#32CD32", // LimeGreen
-    						"#7B68EE", // MediumSlateBlue
-    						"#FF1493"  // DeepPink
-				];
-				userColor: string = "white";
+  currentDate: string = "";
+  currentName: string = "";
+  currentMessage: string = "";
+  userColorArray: string[] = [
+    "#FFA500", // Orange
+    "#FF4500", // Orangered
+    "#FF0000", // Red
+    "#FFFF00", // Yellow
+    "#00FF00", // Lime
+    "#0000FF", // Blue
+    "#8A2BE2", // BlueViolet
+    "#4B0082", // Indigo
+    "#FF69B4", // HotPink
+    "#D2691E", // Chocolate
+    "#FF8C00", // DarkOrange
+    "#ADFF2F", // GreenYellow
+    "#20B2AA", // LightSeaGreen
+    "#FFD700", // Gold
+    "#32CD32", // LimeGreen
+    "#7B68EE", // MediumSlateBlue
+    "#FF1493", // DeepPink
+  ];
+  userColor: string = "white";
 
-				constructor(public settings: SettingsService) {}
+  constructor(public settings: SettingsService) {}
 
+  ngOnInit() {
+    const splitMessage = this.message.split(":");
+    this.currentName = splitMessage[0];
+    this.currentMessage = splitMessage[1];
 
-				ngOnInit() {
-								const splitMessage = this.message.split(":");
-								this.currentName = splitMessage[0];
-								this.currentMessage = splitMessage[1];
+    let state = this.settings.getUserColorStatus();
+    if (state == "disabled") {
+      this.userColor = "white";
+    } else {
+      this.userColor =
+        this.userColorArray[
+          Math.floor(Math.random() * this.userColorArray.length)
+        ];
+    }
 
-								let state = this.settings.getUserColorStatus();
-								if (state == "disabled") {
-												this.userColor = "white";
-								}else {
-												this.userColor = this.userColorArray[Math.floor(Math.random() * this.userColorArray.length)];
-								}
+    let timestampFormat = this.settings.getUserTimestampFormat();
+    if (timestampFormat == undefined) {
+      timestampFormat = "h:mm";
+    }
+    console.log(timestampFormat);
 
-								let timestampFormat = this.settings.getUserTimestampFormat();
-								if (timestampFormat == undefined) { timestampFormat === "h:mm"; }
+    let hours = "";
+    if (Number(new Date().getHours()) < 10) {
+      hours = new Date().getHours() + ":" + "0";
+    } else {
+      hours = new Date().getHours().toString() + ":";
+    }
+    if (timestampFormat !== "disabled") {
+      this.currentDate = hours + new Date().getMinutes();
+    }
 
-								let hours = "";
-								if (Number(new Date().getHours()) < 10) {
-												hours = new Date().getHours()  + ":" + "0";
-								}else {
-												hours = new Date().getHours().toString() + ":";
-								}
-								if (timestampFormat !== "disabled") {
-												this.currentDate = hours + new Date().getMinutes();
-								}
-
-								if (timestampFormat == "h:mm:ss") {
-												this.currentDate += ":" + new Date().getSeconds() + " ";
-								}else if (timestampFormat == "h:mm") {
-												this.currentDate += " ";
-								}
-
-				}
-
-
+    if (timestampFormat == "h:mm:ss") {
+      this.currentDate += ":" + new Date().getSeconds() + " ";
+    } else if (timestampFormat == "h:mm") {
+      this.currentDate += " ";
+    }
+  }
 }
