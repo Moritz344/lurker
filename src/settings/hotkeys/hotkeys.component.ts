0f1ba858aa3c183b433293f384a8e09d05da8cc6
Moritz344 <moritzmaier34@gmail.com>
2026-04-30 11:26:30 +0200

    add hotkey table for hotkey tab in settings

--- src/settings/hotkeys/hotkeys.component.ts
+++ src/settings/hotkeys/hotkeys.component.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-hotkeys',
+  selector: "app-hotkeys",
   imports: [],
-  templateUrl: './hotkeys.component.html',
-  styleUrl: './hotkeys.component.css'
+  templateUrl: "./hotkeys.component.html",
+  styleUrl: "./hotkeys.component.css",
 })
-export class HotkeysComponent {
-
-}
+export class HotkeysComponent {}
